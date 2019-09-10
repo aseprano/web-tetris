40b34907a8fbdde8d453f743ade482d78c380b5f
Antonio Seprano <antonio.seprano@docebo.com>
2019-09-10 09:58:55 +0200

    Tetramino renamed to Tetromino

--- src/Matrix.ts
+++ src/Matrix.ts
@@ -1,8 +1,18 @@
-import { Tetramino } from './Tetramino';
+import { Tetromino } from './Tetromino';
 import { Cell } from './Cell';
 
+/**
+ * Represents an observer of the Matrix.
+ */
 export interface MatrixObserver {
 
+    /**
+     * Main method of the matrix observer.
+     * Gets invoked whenever the occupation status of the matrix changes.
+     * 
+     * @param matrix The matrix that has changed. 
+     * @param newPixels A copy of the current matrix status.
+     */
     matrixDidUpdate(matrix: Matrix, newPixels: Cell[][]): void;
 
 }
@@ -10,39 +20,40 @@ export interface MatrixObserver {
 export interface Matrix {
 
     /**
-     * Inserts a new tetraimino as the current piece.
-     * The new tetraimo is put on the topomost row, in the center.
-     * Returns false if the new tetramino collides.
+     * Inserts a new tetroimino as the current block.
+     * The new tetromino is put in the middle cell of the topmost row, even if it overlaps
+     * other pixels.
+     * Returns false if the newly placed tetromino overlaps.
      * 
-     * @param newTetramino 
+     * @param newTetromino 
      */
-    insert(newTetramino: Tetramino): boolean;
+    insert(newtetromino: Tetromino): boolean;
 
     /**
-     * Tries to rotate the current tetramino left.
-     * If the tetramino cannot rotate, nothing happens.
+     * Tries to rotate the current tetromino left.
+     * If the tetromino cannot rotate, nothing happens.
      */
     rotateLeft(): void;
 
     /**
-     * Tries to rotate the current tetramino right.
-     * If the tetramino cannot rotate, nothing happens.
+     * Tries to rotate the current tetromino right.
+     * If the tetromino cannot rotate, nothing happens.
      */
     rotateRight(): void;
 
     /**
-     * Tries to move the current tetramino on the next row.
-     * Returns true if the current tetramino is moved down, false otherwise.
+     * Tries to move the current tetromino one row down.
+     * Returns true if the current tetromino has been moved down, false otherwise.
      */
     moveDown(): boolean;
 
     /**
-     * Returns the full 10x20 copy of the cells matrix
+     * Returns a full copy of the 10x20 cells matrix
      */
     getPixels(): Cell[][];
 
     /**
-     * Adds an item to the list of observers.
+     * Adds an observer to the list of observers.
      * 
      * @param observer
      */
---
+++ src/Tetromino.ts
@@ -0,0 +1,20 @@
+import { Point } from './Point';
+
+export interface Tetromino {
+
+    /**
+     * Rotates the tetromino left
+     */
+    rotateLeft(): void;
+
+    /**
+     * Rotates the tetromino right
+     */
+    rotateRight(): void;
+
+    /**
+     * Returns the list of points of the current tetromino rotation
+     */
+    getPoints(): Point[];
+
+}
